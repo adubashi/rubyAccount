c6c29d7bbf2fc4f1449ba305475cb1fa7c8939c8
adubashi <adubashi@gmail.com>
2014-06-11 15:49:58 -0700

    New Refactoring of the code

--- companyClass.rb
+++ companyClass.rb
@@ -1,14 +1,48 @@
 class Company 
-	def initialize(totalLiabilities=0,totalAssets = 0, curAssets = 0, curLiabilities = 0, cash = 0, accountsReceivable = 0, shortTermInvestments = 0, 
-		inventories = 0, netIncome = 0, dividendsOnPrefStock = 0, avgOutstandingShares = 0, marketValuePerShare = 0, shareholdersEquity = 0)
+	
+	attr_accessor :totalLiabilities
+	attr_accessor :totalAssets
+	#Current Assets 
+	attr_accessor :cash
+	attr_accessor :accountsReceivable
+	attr_accessor :shortTermInvestments
+	attr_accessor :inventories
+	#Current Liabilities 
+	attr_accessor :shortTermDebt
+	attr_accessor :accountsPayable
+	attr_accessor :accruedLiabilities
+	#Stock Qualities 
+	attr_accessor :netIncome
+	attr_accessor :dividendsOnPrefStock
+	attr_accessor :avgOutstandingShares
+	attr_accessor :marketValuePerShare
+	attr_accessor :shareholdersEquity
+
+	def initialize( totalLiabilities = 0, totalAssets = 0, 
+					#Current Assets 
+					cash = 0, accountsReceivable = 0, shortTermInvestments = 0, inventories = 0, 
+					#Current Liabiilities 
+					shortTermDebt = 0, accountsPayable = 0, accruedLiabilities = 0
+					
+
+					netIncome = 0, 
+					dividendsOnPrefStock = 0, avgOutstandingShares = 0, marketValuePerShare = 0, shareholdersEquity = 0)
 		@totalLiabilities = totalLiabilities
 		@totalAssets = totalAssets
-		@curAssets = curAssets
-		@curLiabilities = curLiabilities
+		
+		
+		#Current Assets
 		@cash = cash
 		@accountsReceivable = accountsReceivable
 		@shortTermInvestments = shortTermInvestments
 		@inventories = inventories
+		
+		#Current Liabilities 
+		@shortTermDebt = shortTermDebt
+		@accountsPayable = accountsPayable
+		@accruedLiabilities = accruedLiabilities
+
+
 		@netIncome = netIncome
 		@dividendsOnPrefStock = dividendsOnPrefStock
 		@avgOutstandingShares = avgOutstandingShares
@@ -16,14 +50,24 @@ class Company
 		@shareholdersEquity = shareholdersEquity
 	end 
 
-	 
+	
 
-	def quickRatio
-		return (@curAssets/@curLiabilities)
+
+
+
+
+
+	def currentAssets
+		return @cash + @accountsReceivable + @shortTermInvestments + @inventories
 	end 
+	 
+	def currentLiabilities
+		return @shortTermDebt + @accountsPayable + @accruedLiabilities
+	end 
+ 
 
 	def acidTest
-		quickRatio = (@cash + @accountsReceivable + @shortTermInvestments)/(@curLiabilities)
+		quickRatio = (self.currentAssets/self.currentLiabilities)
 		return quickRatio
 	end 
 
@@ -43,5 +87,5 @@ class Company
 end 
 
 
-a = Company.new(0,2,5,25)
+
 
